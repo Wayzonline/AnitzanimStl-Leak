5ef7936936d05775b439fcc9676f9ca07df6fd32
Wayz <130591428+Wayzonline@users.noreply.github.com>
2024-01-31 23:18:35 +0100

    aa

---
+++ test2/Program.cs
@@ -0,0 +1,13 @@
+﻿using Colorful;
+
+namespace test2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // had Fake Shit Here Regards :)
+            Colorful.Colorful.WriteLine();
+        }
+    }
+}
